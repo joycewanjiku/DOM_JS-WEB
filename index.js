e3bbe8c8ac16e293eb0f02dba306f1668c472813
student <joycemumbiiwanjiku@gmail.com>
2023-05-30 06:35:10 +0300

    DOM_JS_WEBSITE

---
+++ index.js
@@ -0,0 +1,29 @@
+// Create header element
+const header = document.createElement('header');
+header.classList.add('header');
+
+// Create navigation bar element
+const nav = document.createElement('nav');
+nav.classList.add('nav');
+
+// Create section elements
+const section1 = document.createElement('section');
+section1.classList.add('section1');
+
+const section2 = document.createElement('section');
+section2.classList.add('section2');
+
+const section3 = document.createElement('section');
+section3.classList.add('section3');
+
+// Create footer element
+const footer = document.createElement('footer');
+footer.classList.add('footer');
+
+// Append elements to the body of the webpage
+document.body.appendChild(header);
+document.body.appendChild(nav);
+document.body.appendChild(section1);
+document.body.appendChild(section2);
+document.body.appendChild(section3);
+document.body.appendChild(footer);
