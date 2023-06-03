82f43a22c94aaff091f07b29ee2fea792d91ac84
student <joycemumbiiwanjiku@gmail.com>
2023-06-03 21:58:12 +0300

    DOM_JS_WEBSITE

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 function generateProductCards(data) {
-    const productContainer = document.getElementById("products");
-    data.forEach((product) => {
-      const card = document.createElement("div");
-      card.classList.add("product-card");
-      card.innerHTML = `
-              <img class= "productImage" src="${product.thumbnail}" alt="${product.name}" />
-              <h2>${product.title}</h2>
-              <div class='productPrice'>
-              <p>${product.price}</p>
-              <button>ADD TO CART</button>
-              </div>
-          `;
-      productContainer.appendChild(card);
+  const Products = document.getElementById("Products");
+  // iterate through the data and create a card for each product
+  data.forEach((product) => {
+    // create a card element
+    const card = document.createElement("div");
+    card.classList.add("product-card");
+    // Set the card content using the product data
+    card.innerHTML = `
+            <img src="${product.thumbnail}" alt="${product.name}" />
+            <h2>${product.title}</h2>
+            <div class='productPrice'>
+            <p>${product.price}</p>
+            <button>ADD TO CART</button>
+            </div>
+        `;
+    // append the card to the product container
+    Products.appendChild(card);
+  });
+}
+function fetchProducts() {
+  fetch("https://dummyjson.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+      // Get the first 8 products from the data
+      const returnedData = data.products.slice(0, 8);
+      // call the function to generate cards
+      generateProductCards(returnedData);
     });
-  }
-  function fetchProducts() {
-    fetch("https://dummyjson.com/")
-      .then((response) => response.json())
-      .then((data) => {
-        const returnedData = data.products.slice(0, 10);
-        generateProductCards(returnedData);
-    });
-  }
-  fetchProducts()
+}
+fetchProducts()
+
+
+
+
+
